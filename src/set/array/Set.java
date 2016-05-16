5dec95762739eaceb34cdcb40106c66802d7c777
rambrundavanam <rambrun@indiana.edu>
2016-05-16 11:19:35 -0400

    Add final project

---
+++ src/set/array/Set.java
@@ -0,0 +1,103 @@
+package set.array;
+
+public class Set<T> {
+	private T[] arrayElement;
+	int size;
+
+	public Set() {
+		this.arrayElement = null;
+		size=0;
+	}
+
+	public Set(T[] element) {
+		arrayElement = element;
+		size = arrayElement.length;
+	}
+
+	@SuppressWarnings("unchecked")
+	public void add(T element) {
+		if(arrayElement==null){
+			arrayElement=(T[]) new Object[10];
+			arrayElement[size++] = element;
+		}
+		else{
+			if (!contains(element)) {
+				if (size == arrayElement.length) {
+					incrementArray();
+				}
+				arrayElement[size++] = element;
+			}
+			
+		}
+	}
+
+  	@SuppressWarnings("unchecked")
+	public void incrementArray(){
+		T[] temp = arrayElement;
+		int tempSize = size *2;
+		arrayElement = (T[]) new Object[tempSize];
+		System.arraycopy(temp, 0, arrayElement, 0, size);
+	}
+	
+    
+  	public void remove(){
+  		if(arrayElement!=null){
+  			if(arrayElement.length/4>=size && arrayElement.length>10){
+  				decrementArray();
+  			}
+  			arrayElement[size-1]=null;
+  			size=size-1;
+  		}
+  	}
+  	@SuppressWarnings("unchecked")
+	public void decrementArray(){
+  		T[] temp =arrayElement;
+  		int tempSize=arrayElement.length/2;
+  		arrayElement=(T[]) new Object[tempSize];
+  		System.arraycopy(temp, 0, arrayElement, 0, size);
+  	}
+  	public boolean contains(T element){
+		if(element == null){
+			for(int i =0;i<size;i++){
+				if(arrayElement[i]==null)return true;
+			}
+		}else{
+			for(int i=0;i<size;i++){
+				if(element.equals(arrayElement[i])){
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
+	public int size(){
+		if(arrayElement !=null){
+			return arrayElement.length;
+		}else return 0;
+	}
+	
+	public void clear(){
+		arrayElement = null;
+	}
+	
+	public String toString(){
+		String toString;
+		if(arrayElement == null || arrayElement.length == 0){
+			return"[Empty]";
+		}else{
+			toString = "[";
+			for(int i=0;i<arrayElement.length;i++){
+				toString += arrayElement[i]+",";
+			}
+			toString += "]";
+		}
+		return toString;
+	}
+	
+	public boolean isEmpty(){
+		if(arrayElement == null || arrayElement.length==0){
+			return true;
+		}else return false;
+	}
+}
---
+++ src/set/array/Test.java
@@ -0,0 +1,36 @@
+package set.array;
+
+import set.array.Set;
+
+
+class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Set<Integer> intArr=new Set<Integer>();
+		System.out.println(intArr.toString());
+		System.out.println("ActualSize: "+intArr.size+" "+"Length: "+intArr.size() );
+		for(int i=0;i<12;i++){
+			intArr.add(i);
+		}
+		System.out.println("ActualSize: "+intArr.size+" "+"Length: "+intArr.size() );
+		System.out.println(intArr.toString());
+		System.out.println("ActualSize: "+intArr.size+" "+"Length: "+intArr.size() );
+		intArr.remove();
+		intArr.remove();
+		intArr.remove();
+		intArr.remove();
+		intArr.remove();
+		intArr.remove();
+		System.out.println(intArr.toString());
+		intArr.remove();
+		System.out.println(intArr.toString());
+		intArr.remove();
+		System.out.println(intArr.toString());
+		intArr.remove();
+		System.out.println(intArr.toString());
+		System.out.println("ActualSize: "+intArr.size+" "+"Length: "+intArr.size() );
+	}
+
+}
