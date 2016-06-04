45e1dbd2023092d70b3aed32c07201aabd818966
Ram <ram8214@yahoo.com>
2016-06-04 15:59:58 -0400

    improving the performance

--- src/set/array/MostFrequentWords.java
+++ src/set/array/MostFrequentWords.java
@@ -1,14 +1,16 @@
+package set.array;
 import java.io.*;
 import java.util.*;
 
 //Shows the 5 most frequent words in the List of words
 public class MostFrequentWords {
+	
 	public static void main(String[] args) {
 		String s = "Welcome to the world of Geeks This portal has been created to provide well written well thought and well explained solutions for selected questions If you like Geeks for Geeks and would like to contribute here is your chance You can write article and mail your article to contribute at geeksforgeeks org See your article appearing on the Geeks for Geeks main page and help thousands of other Geeks";
 		String[] array = s.split(" ");
 		int k = 5;
-		ArrayList<Words> al = new ArrayList<Words>();
 		HashMap<String, Integer> hm = new HashMap<String, Integer>();
+		PriorityQueue<Words> pq = new PriorityQueue<Words>();
 		for (String string : array) {
 			if (hm.containsKey(string)) {
 				hm.put(string, hm.get(string) + 1);
@@ -17,16 +19,15 @@ public class MostFrequentWords {
 			}
 		}
 		for (Map.Entry<String, Integer> entry : hm.entrySet()) {
-			al.add(new Words(entry.getKey(), entry.getValue()));
+			pq.add(new Words(entry.getKey(), entry.getValue()));
 		}
-		Collections.sort(al, new Words());
 		for (int i = 0; i < 5; i++) {
-			System.out.println(al.get(i).word + " " + al.get(i).value);
+			System.out.println(pq.poll().toString());
 		}
 	}
 }
 
-class Words implements Comparator<Words> {
+class Words implements Comparable<Words> {
 
 	String word;
 	int value;
@@ -39,11 +40,15 @@ class Words implements Comparator<Words> {
 	public Words() {
 	}
 
-	public int compare(Words o1, Words o2) {
-		if (o1.value >= o2.value) {
+	public int compareTo(Words o) {
+		if (this.value >= o.value) {
 			return -1;
 		} else {
 			return 1;
 		}
+	}
+	
+	public String toString(){
+		return this.word+" : "+this.value;
 	}
 }
